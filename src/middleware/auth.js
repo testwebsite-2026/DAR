403a9d7590a7144ea5355b603385ce46422d1462
testwebsite-2026 <rievenimmersion@gmail.com>
2026-09-14 20:15:25 +0800

    Delete src/middleware/auth.js

--- src/middleware/auth.js
+++
@@ -1,8 +0,0 @@
-function requireAuth(req, res, next) {
-  if (req.session && req.session.username) {
-    return next();
-  }
-  return res.status(401).json({ error: 'Not authenticated.' });
-}
-
-module.exports = { requireAuth };
